cf71d7b0242d9cbd73b4ad0236d99ce007f91d7a
Cameron Lee <cameronslee@hotmail.com>
2024-05-30 12:04:29 -0700

    feat: basic search and scrape

--- ohgolly.go
+++ ohgolly.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+const DDG string = "https://html.duckduckgo.com/html/?q=golang"
 
 func main() {
-	fmt.Println("Hello, World!")
+	//search_query := ""
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("html.duckduckgo.com"),
+		colly.Async(true),
+	)
+
+	fmt.Print("FOO\n")
+	// On every a element which has href attribute call callback
+	c.OnHTML("h2.result__title a.result__a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping
+	c.Visit(DDG)
+
+	c.Wait()
 }
